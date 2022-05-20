5dc54495677c1ef5e5ef53ba86b98506b7e74231
thisitha <thisithakavinda@gmail.com>
2022-05-20 13:38:29 +0530

    Created the Home pag UI

---
+++ app/src/main/java/com/kavinda/ogilvytestapp/timerActivity.java
@@ -0,0 +1,14 @@
+package com.kavinda.ogilvytestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class timerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
